4c04c4d42f3579a8b4cc2e6eb285c97fc67ca5eb
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2023-11-26 13:59:04 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,23 @@ var swiper = new Swiper(".slide-video", {
         nextEl: ".s-videos .btn-next",
         prevEl: ".s-videos .btn-prev",
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.1,
+        paceBetween: 16,
+      },
+
+      767: {
+        slidesPerView: 2.1,
+      },
+
+      1024: {
+        slidesPerView: 3.1,
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    }
 });
